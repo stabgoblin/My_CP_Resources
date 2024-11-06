75378c95cb0c24a0365208553a9d89354a98a043
Amrit <amrit71082@gmail.com>
2024-11-06 23:01:21 +0530

    Add files via upload

---
+++ my_template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define ull unsigned long long
+#define ll long long
+#define vi vector<int>
+#define vl vector<long>
+#define pb push_back
+#define endl "\n"
+#define sp <<" "
+#define str string
+
+using namespace std;
+ll bsearch(vi arr,int l,int r,int key){
+//	work in progress
+	return -1;
+}
+
+
+
+void solve(){
+	//solution here
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+//    ifstream cin("input.txt");
+//    ofstream cout("output.txt");
+//	add these two lines above when input output 
+//	from and to a file
+	int t=1;
+//	cin>>t; //if multiple test cases
+	while(t--){
+		solve();
+	}
+}
