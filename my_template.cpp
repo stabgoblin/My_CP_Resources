6b84906a9225bc0929005e78e008ad1fd497de2e
Amrit <amrit71082@gmail.com>
2025-06-04 19:57:07 +0530

    Update my_template.cpp

--- my_template.cpp
+++ my_template.cpp
@@ -3,27 +3,42 @@
 #define ull unsigned long long
 #define ll long long
 #define vi vector<int>
-#define vb vector<bool>
 #define pii pair<int,int>
-#define vull vector<unsigned long long>
 #define pb push_back
 #define mp make_pair
 #define fwdi(a,b) for(int i=a;i<b;i++)
 #define fwdj(a,b) for(int j=a;j<b;j++)
 #define el "\n"
 #define sp <<" "
-#define co cout<<
-#define ci cin>>
 #define all(arr) arr.begin(),arr.end()
-#define rall(arr) arr.rbegin(),arr.rend() 
-
+#define rall(arr) arr.rbegin(),arr.rend()
 using namespace std;
 
-template <typename T>
-T mod(T a, T b) {
-    return ((a % b) + b) % b;
+template<typename T>
+T MOD(T a, T m) {
+    return (a % m + m) % m;
 }
 
+long long binpow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+template <typename T>
+T cdiv(T a, T b) {
+    T q = a / b;
+    T r = a % b;
+    if (r != 0 && !((a < 0) ^ (b < 0))) {
+        q++;
+    }
+    return q;
+}
 template <typename T>
 T fdiv(T a, T b) {
     T q = a / b;
@@ -34,25 +49,10 @@ T fdiv(T a, T b) {
     return q;
 }
 
-template <typename T>
-T cdiv(T a, T b) {
-    T q = a / b;
-    T r = a % b;
-    if (r != 0 && !((a < 0) ^ (b < 0))) {
-        q++;
-    }
-    return q;
-}
-
-int d(int n){int ans = 0;int i=1;while(i*i<n){if(n%i == 0){ans++;ans++;}i++;}if(i*i == n){ans++;}return ans;}
-// const int MAXN = 1e6+1;
-// bool is_prime[MAXN];
-
-// void seive(){fill(is_prime,is_prime + MAXN,true);is_prime[0] = false; is_prime[1] = false;for(int i = 2;i*i<MAXN;i++){if(is_prime[i]){for(int j = i*i;j<MAXN;j += i){is_prime[j] = false;}}}}
+// vector<int> primes;
+// void seive(vector<int>& primes){ull nsqrt =  1e6;vector<char> is_prime(nsqrt + 2, true);for (ull i = 2; i <= nsqrt; i++) {if (is_prime[i]) {primes.push_back(i);for (ull j = i * i; j <= nsqrt; j += i)is_prime[j] = false;}}}
 
 void solve(){
-    // ifstream cin("input.txt");
-    // ofstream cout("output.txt");
     
 }
 
@@ -60,8 +60,8 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     int t=1;
-    cin>>t; //if multiple test cases
-    
+    cin >> t;
+    // seive(primes);
     for(int z = 0;z<t;z++){
         solve();
     }
